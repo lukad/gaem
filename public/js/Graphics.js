1ef12901f09ea8cf4f1f42dab74624a44d4946c7
Colin <colin.sames@haw-hamburg.de>
2015-02-21 10:49:16 +0100

    Prepared Graphics.

--- public/js/Graphics.js
+++ public/js/Graphics.js
@@ -4,12 +4,16 @@
 
 define("Graphics", function () {
 
-	function Graphics() {
-	  
+	function Graphics(ctx) {
+		var this.ctx = ctx;
+		var start_button = new Image(200, 75);
+		start_button.src = "res/start.png";
 	};
 
 	Graphics.prototype.draw_game_menu = function() {
-	 console.log("Graphics:draw_game_menu");
+	 	console.log("Graphics:draw_game_menu");
+		ctx.clearRect(0, 0, ctx.);
+		ctx.drawImage(start_button, ctx.getWidth / 3, ctx.getHeight / 3);
 	};
 
 	Graphics.prototype.draw = function() {
