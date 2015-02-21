136554a2afebf984b82901b26cefa6fefad15d7b
ulfbiallas <ulfbiallas@googlemail.com>
2015-02-21 15:33:56 +0100

    fixed speed in view

--- public/js/Graphics.js
+++ public/js/Graphics.js
@@ -10,29 +10,49 @@ define("Graphics", ['Button', 'Song'], function(Button, Song) {
 		this.song = new Song();
 		this.width = width;
 		this.height = height;
+		this.buttons = [
+			new Button("start", width / 3, height / 3, 200, 75),
+			new Button("credits", width / 3, 0.6 * height, 200, 75)
+		];
+		document.addEventListener("mousedown", this.onMouseDown.bind(this));
 	}
+
+	Graphics.prototype.drawGameMenu = function() {
+		for(i = this.buttons.length - 1; i >= 0; i--) {
+			this.buttons[i].draw(this.ctx);
+		}
+	};
+
 
 	Graphics.prototype.draw = function() {
 		var currentTime = this.song.getCurrentTime();
+		console.log(currentTime);
 		var tracks = this.song.getTracks();
 		var columns = tracks.length;
 		var lineY = 400;
+		var zoom = 0.1;
+		var pxPerMs = 0.05;
 		
+		this.ctx.fillText("time: " + Math.round(currentTime),10,10);
+
 		for (var i=0; i<columns; ++i) {
 			for (j=0; j<tracks[i].length; ++j) {
 				var note = tracks[i][j];
-				var start = note.start+lineY;
-
-					if(currentTime >= start && currentTime <= start + note.duration) {
-						this.ctx.fillStyle="#000000";
-						this.ctx.shadowColor = '#ff0000';
-						this.ctx.shadowBlur = 20;
-					} else {
-						this.ctx.fillStyle="#555555";
-						this.ctx.shadowBlur = 0;
-					}
-					
-					this.ctx.fillRect(100+50*i, lineY-note.duration+currentTime-start, 20, note.duration);
+				var start = note.start;
+				
+				if(currentTime >= start && currentTime <= start + note.duration) {
+					this.ctx.fillStyle="#000000";
+					this.ctx.shadowColor = '#ff0000';
+					this.ctx.shadowBlur = 5;
+				} else {
+					this.ctx.fillStyle="#555555";
+					this.ctx.shadowBlur = 0;
+				}
+			
+				msLeftToBePlayed = Math.round(start-currentTime);
+				rectY = lineY - (msLeftToBePlayed+note.duration)*pxPerMs;
+			
+				this.ctx.fillRect(100+50*i, rectY, 100*pxPerMs, note.duration*pxPerMs);
 
 			}
 		}
